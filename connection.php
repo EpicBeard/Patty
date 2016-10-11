2ef2adc0f03d31790e63857aaa4bb8b2450aaea1
Bearded One <tommygordon103@gmail.com>
2016-10-11 14:08:11 -0700

    uploading files
    These are just from the beginning....somewhat working but will be more descriptive on future pushes

---
+++ connection.php
@@ -0,0 +1,69 @@
+<?php
+
+date_default_timezone_set('America/New_York');
+
+require_once ("../vendor/autoload.php");
+
+$log = new \Monolog\Logger('PHRETS');
+$log->pushHandler(new \Monolog\Handler\StreamHandler('php://stdout', \Monolog\Logger::DEBUG));
+
+$config = new \PHRETS\Configuration;
+
+//$config->setLoginUrl('http://www.idxseodemo.com/workarea/run_now.php');
+
+$config->setLoginUrl('http://cat.rets.paragonrels.com/rets/fnisrets.aspx/CAT/login?rets-version=rets/1.5')
+        ->setUsername('thomasgordon')
+        ->setPassword('dRa#2fUr')
+        ->setRetsVersion('1.7.2');
+
+$rets = new \PHRETS\Session($config);
+//$rets->setLogger($log);
+
+$connect = $rets->Login();
+
+$system = $rets->GetSystemMetadata();
+//var_dump($system);
+
+$resources = $system->getResources();
+$classes = $resources->first()->getClasses();
+//var_dump($classes);
+
+$classes = $rets->GetClassesMetadata('Property');
+//var_dump($classes->first());
+
+$objects = $rets->GetObject('Property', 'Photo', '00-1669', '*', 1);
+//var_dump($objects);
+
+$fields = $rets->GetTableMetadata('Property', 'LA_2', 'Lakefront_Lkp_2=Y');
+//var_dump($fields[0]);
+
+//echo "<pre>";
+//print_r($objects);
+//echo "</pre>";
+
+
+$results = $rets->Search('Property', 'SF_1', 'LM_char10_34=|Y');
+
+
+echo "<pre>";
+print_r($results->toJSON());
+echo "</pre>";
+
+
+//$photos = $rets->GetObject("Property", "Photo", "9577056", "*", 0);
+//foreach ($photos as $photo) {
+//    $photo = $photo->getContent();
+//    if($photo){
+//        echo "<hr><pre>";
+//        var_dump($photo);
+//        echo "</pre><hr>";
+//    }
+//}
+
+//echo $items->getTotalResultsCount();
+
+//$db = pg_connect("host=localhost port=5432 dbname=Houses user=postgres password=AMU5uck5");
+//$query = "INSERT INTO houses VALUES ('$_POST[L_ListingID]','$_POST[L_Type]','$_POST[L_Address]')";
+//$result = pg_query($query);
+
+?>
---
+++ logsearch.php
@@ -0,0 +1,34 @@
+<?php
+
+include('../vendor/troydavisson/phrets/src/Configuration.php');
+
+$rets_username = "thomasgordon";
+$rets_password = "dRa#2fUr";
+$rets_user_agent_password = "dRa#2fUr";
+$rets_login_url = "http://cat.rets.paragonrels.com/rets/fnisrets.aspx/CAT/login?rets-version=rets/1.5";
+
+$config = new \PHRETS\Configuration;
+$config->setLoginUrl($rets_login_url);
+$config->setUsername($rets_username);
+$config->setPassword($rets_password);
+
+// optional.  value shown below are the defaults used when not overridden
+$config->setRetsVersion('1.7.2'); // see constants from \PHRETS\Versions\RETSVersion
+$config->setUserAgent('PHRETS/2.0');
+$config->setUserAgentPassword($rets_user_agent_password); // string password, if given
+$config->setHttpAuthenticationMethod('digest'); // or 'basic' if required
+$config->setOption('use_post_method', false); // boolean
+$config->setOption('disable_follow_location', false); // boolean
+
+
+$rets = new \PHRETS\Session($config);
+
+$bulletin = $rets->Login();
+
+//$results = $rets->Search($resource, $class, $query);
+
+// or with the additional options (with defaults shown)
+
+
+
+?>
